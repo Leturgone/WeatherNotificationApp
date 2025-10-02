3afe72ab99f2360ca361124cdbf901c474cb7625
Leturgone <finekon221@gmail.com>
2025-10-02 12:53:25 +0300

    added resylt to repository

--- app/src/main/java/com/example/weathernotificationapp/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weathernotificationapp/domain/repository/WeatherRepository.kt
@@ -3,5 +3,5 @@ package com.example.weathernotificationapp.domain.repository
 import com.example.weathernotificationapp.domain.model.Weather
 
 interface WeatherRepository {
-    suspend fun getWeather(city: String): Weather
+    suspend fun getWeather(city: String): Result<Weather>
 }
