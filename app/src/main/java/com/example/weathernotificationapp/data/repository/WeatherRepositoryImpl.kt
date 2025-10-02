8968af3dbb1b814abbdd284b344a6b95b38b0be8
Leturgone <finekon221@gmail.com>
2025-10-02 12:53:37 +0300

    implemented repository

--- app/src/main/java/com/example/weathernotificationapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weathernotificationapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package com.example.weathernotificationapp.data.repository
 
-class WeatherRepositoryImpl {
+import android.util.Log
+import com.example.example.WeatherResponse
+import com.example.weathernotificationapp.data.api.WeatherApi
+import com.example.weathernotificationapp.domain.model.Weather
+import com.example.weathernotificationapp.domain.repository.WeatherRepository
+import kotlinx.coroutines.runBlocking
+import kotlin.math.roundToInt
+
+class WeatherRepositoryImpl(private val api: WeatherApi) : WeatherRepository{
+
+    private fun WeatherResponse.convert(): Weather{
+        return Weather(
+            main?.temp?.roundToInt(),
+            weather.getOrNull(0)?.main,
+            weather.getOrNull(0)?.description,
+            wind?.speed?.roundToInt()
+        )
+    }
+
+    override suspend fun getWeather(city: String): Result<Weather> {
+        return try {
+            val result = api.getWeather(city).convert()
+            Result.success(result)
+        }catch (e: Exception){
+            Log.e("getWeathe", e.toString())
+            Result.failure(e)
+        }
+    }
+
+
 }
