e46a29aeeec6895bc885229320c62c71fb55c65c
Leturgone <finekon221@gmail.com>
2025-10-02 12:53:53 +0300

    implemented repository

--- app/src/main/java/com/example/weathernotificationapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weathernotificationapp/data/repository/WeatherRepositoryImpl.kt
@@ -24,7 +24,7 @@ class WeatherRepositoryImpl(private val api: WeatherApi) : WeatherRepository{
             val result = api.getWeather(city).convert()
             Result.success(result)
         }catch (e: Exception){
-            Log.e("getWeathe", e.toString())
+            Log.e("getWeather", e.toString())
             Result.failure(e)
         }
     }
